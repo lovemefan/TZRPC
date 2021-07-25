4c1d943cff41592b5cb8951ba053253fb72cbad2
lovemefan <450489712@qq.com>
2021-07-25 13:56:51 +0800

    add superclass of bert

---
+++ bert/BertBase.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Time  : 2021/7/25 13:18
+# @Author : lovemefan
+# @Email : lovemefan@outlook.com
+# @File : BertBase.py
+from abc import abstractmethod, ABCMeta
+
+
+class BertBase(metaclass=ABCMeta):
+    def __init__(self, name: str):
+        self.name = name
+
+    def __str__(self):
+        return f"bert for {self.name}"
+
+    @abstractmethod
+    def output(self, input: str):
+        pass
