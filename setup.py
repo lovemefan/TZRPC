5c8696f57d56509526782e3535f9fe755a9fc37e
lovemefan <lovemefan@outlook.com>
2023-07-31 11:09:19 +0800

    update

--- setup.py
+++ setup.py
@@ -8,26 +8,20 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-requirements = {
-    "install": [
-        "grpcio==1.40.0",
-        "grpcio-tools==1.40.0",
-        "numpy"
-    ]
-}
+requirements = {"install": ["grpcio==1.40.0", "grpcio-tools==1.40.0", "numpy"]}
 
 setuptools.setup(
     name="tzrpc",
     version="0.0.1",
     author="Lovemefan, Yunnan Key Laboratory of Artificial Intelligence, "
-           "Kunming University of Science and Technology, Kunming, Yunnan ",
+    "Kunming University of Science and Technology, Kunming, Yunnan ",
     author_email="lovemefan@outlook.com",
     description="a rpc framework for python base on grpc",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lovemefan/TZRPC",
     packages=setuptools.find_namespace_packages(),
-    install_requires=requirements,
+    install_requires=requirements["install"],
     python_requires=">=3.7.0",
     classifiers=[
         "Programming Language :: Python :: 3.7",
